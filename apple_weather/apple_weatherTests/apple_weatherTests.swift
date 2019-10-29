95d6c369b79ed785f57d3f5f17f52cc4c1acb37b
Maxim Vialyx <maksimvialyx@gmail.com>
2019-10-29 20:50:39 +0300

    - init

---
+++ apple_weather/apple_weatherTests/apple_weatherTests.swift
@@ -0,0 +1,34 @@
+//
+//  apple_weatherTests.swift
+//  apple_weatherTests
+//
+//  Created by Maxim Vialyx on 10/29/19.
+//  Copyright © 2019 Vialyx. All rights reserved.
+//
+
+import XCTest
+@testable import apple_weather
+
+class apple_weatherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
